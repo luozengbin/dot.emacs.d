7e382c9f08c3cc55e046146ce858e861f77a1a31
luozengbin <jalen.cn@gmail.com>
2013-04-21 15:47:42 +0900

    test update

--- site-start.d/init_package.el
+++ site-start.d/init_package.el
@@ -36,7 +36,7 @@
      (expand-file-name "~/.emacs.d/elpa/package.el")))
 
 (require 'package)
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+(setq package-archives '(;;("ELPA" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
